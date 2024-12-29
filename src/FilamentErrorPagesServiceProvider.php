74a2387fb0eb3137bd2800928742fabf1ec11e9b
CodeWithDennis <CodeWithDennis@users.noreply.github.com>
2024-12-29 16:13:31 +0000

    Fix styling

--- src/FilamentErrorPagesServiceProvider.php
+++ src/FilamentErrorPagesServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace Cmsmaxinc\FilamentErrorPages;
 
+use Cmsmaxinc\FilamentErrorPages\Commands\FilamentErrorPagesCommand;
+use Cmsmaxinc\FilamentErrorPages\Testing\TestsFilamentErrorPages;
 use Filament\Support\Assets\AlpineComponent;
 use Filament\Support\Assets\Asset;
 use Filament\Support\Assets\Css;
@@ -13,8 +15,6 @@ use Livewire\Features\SupportTesting\Testable;
 use Spatie\LaravelPackageTools\Commands\InstallCommand;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
-use Cmsmaxinc\FilamentErrorPages\Commands\FilamentErrorPagesCommand;
-use Cmsmaxinc\FilamentErrorPages\Testing\TestsFilamentErrorPages;
 
 class FilamentErrorPagesServiceProvider extends PackageServiceProvider
 {
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,6 +4,7 @@ namespace Cmsmaxinc\FilamentErrorPages\Tests;
 
 use BladeUI\Heroicons\BladeHeroiconsServiceProvider;
 use BladeUI\Icons\BladeIconsServiceProvider;
+use Cmsmaxinc\FilamentErrorPages\FilamentErrorPagesServiceProvider;
 use Filament\Actions\ActionsServiceProvider;
 use Filament\FilamentServiceProvider;
 use Filament\Forms\FormsServiceProvider;
@@ -16,7 +17,6 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 use Livewire\LivewireServiceProvider;
 use Orchestra\Testbench\TestCase as Orchestra;
 use RyanChandler\BladeCaptureDirective\BladeCaptureDirectiveServiceProvider;
-use Cmsmaxinc\FilamentErrorPages\FilamentErrorPagesServiceProvider;
 
 class TestCase extends Orchestra
 {
