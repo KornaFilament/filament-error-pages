56fac22bcdb6a7a8a918b75285fa80bc859e676e
Dennis Elsinga <personal.elsinga@gmail.com>
2025-01-30 15:48:06 +0100

    Add multilingual error page translations

---
+++ resources/lang/de/error-pages.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'home' => 'Zurück zur Startseite',
+    'previous' => 'Zurück zur vorherigen Seite',
+    '404' => [
+        'title' => 'Die gesuchte Seite kann nicht gefunden werden.',
+        'description' => 'Die gesuchte Seite wurde möglicherweise entfernt, umbenannt oder ist vorübergehend nicht verfügbar.',
+    ],
+    '403' => [
+        'title' => 'Die gesuchte Seite ist nicht zugänglich.',
+        'description' => 'Sie haben keine Berechtigung, um die gesuchte Seite aufzurufen.',
+    ],
+];
