4fe72c77e384f346f72e8af231374316ccaa7fdb
Majd <majd@oyounmedia.com>
2025-02-08 17:46:14 +0200

    add arabic translationsg

---
+++ resources/lang/ar/error-pages.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'home' => 'العودة إلى الصفحة الرئيسية',
+    'previous' => 'العودة إلى الصفحة السابقة',
+    '404' => [
+        'title' => 'الصفحة التي تبحث عنها غير موجودة.',
+        //The page you\'re looking for might have been removed, had its name changed, or is temporarily unavailable.
+        'description' => 'الصفحة التي تبحث عنها ربما تمت إزالتها أو تغيير اسمها أو أنها غير متاحة مؤقتاً.',
+    ],
+    '403' => [
+        'title' => 'ليس لديك إذن للوصول إلى هذه الصفحة.',
+        'description' => 'تأكد من أن لديك صلاحيات الوصول لهذا الصفحة وحاول مرة أخرى.',
+    ],
+];
